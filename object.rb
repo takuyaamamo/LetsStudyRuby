91de88a46e7e3a4f2fe9bbac4545180c50f14d8e
ama <takuyaamano@amamo.local>
2019-04-05 14:25:27 +0900

    [start][finish]2-6

---
+++ object.rb
@@ -0,0 +1,24 @@
+# 数値
+puts 100
+puts 100 + 5
+
+# 文字列
+puts "Rubyを学習"
+name = "鈴木さん"
+puts "Hello" + name
+puts "こんにちは#{name}"
+
+# 配列
+names = ["Perl", "Ruby", "Python", "Swift"]
+puts names
+puts names[1]
+
+# ハッシュ
+tall = {"太郎"=>185, "二郎"=>170, "花子"=>150}
+puts tall
+puts tall["太郎"]
+
+# シンボル
+tall = {:太郎=>285, :二郎=>270, :花子=>250}
+puts tall
+puts tall[:花子]
